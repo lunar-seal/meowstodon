7b77a181160ebb9215136e23396604d5d04f91bc
likeazir <code@langj.dev>
2026-05-31 18:33:27 +0200

    Revert "preserve spoilers in threadfragment"
    An upstream fix for this is available, so drop the workaround.
    
    This reverts commit b3916e3aa1230aafe9406332b76efd51a0c1e8b6.

--- mastodon/src/main/java/org/joinmastodon/android/fragments/BaseStatusListFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/BaseStatusListFragment.java
@@ -651,18 +651,7 @@ public abstract class BaseStatusListFragment<T extends DisplayItemsParent> exten
 	}
 
 	private void toggleSpoiler(Status status, SpoilerStatusDisplayItem spoilerItem){
-		int index=-1;
-		for(int i=0;i<displayItems.size();i++){
-			if(displayItems.get(i) instanceof SpoilerStatusDisplayItem s
-					&& s.parentID.equals(spoilerItem.parentID)
-					&& s.spoilerType==spoilerItem.spoilerType){
-				index=i;
-				spoilerItem=s;
-				break;
-			}
-		}
-		if(index<0)
-			return;
+		int index=displayItems.indexOf(spoilerItem);
 		if(status.revealedSpoilers.contains(spoilerItem.spoilerType)){
 			int itemCount=spoilerItem.contentItems.size();
 			displayItems.addAll(index+1, spoilerItem.contentItems);
