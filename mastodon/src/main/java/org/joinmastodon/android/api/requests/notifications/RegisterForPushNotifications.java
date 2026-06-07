c0322357018d2333024544ca557567e714addefd
lunar-seal <code@langj.dev>
2026-06-07 22:54:02 +0200

    fix: make push notifications work with own server

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/notifications/RegisterForPushNotifications.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/notifications/RegisterForPushNotifications.java
@@ -7,7 +7,7 @@ public class RegisterForPushNotifications extends MastodonAPIRequest<PushSubscri
 	public RegisterForPushNotifications(String deviceToken, String encryptionKey, String authKey, PushSubscription.Alerts alerts, PushSubscription.Policy policy, String accountID){
 		super(HttpMethod.POST, "/push/subscription", PushSubscription.class);
 		Request r=new Request();
-		r.subscription.endpoint="https://app.joinmastodon.org/relay-to/fcm/"+deviceToken+"/"+accountID;
+		r.subscription.endpoint="https://push.ff15.eu/relay-to/fcm/"+deviceToken+"/"+accountID;
 		r.data.alerts=alerts;
 		r.policy=policy;
 		r.subscription.keys.p256dh=encryptionKey;
